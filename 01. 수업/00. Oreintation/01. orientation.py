cecda75c5939dcac625e304c43eb51f3e006cc6b
shifttag <ehdgus0837@naver.com>
2026-06-19 09:02:03 +0900

    01. orientation 26.06.18

---
+++ 01. 수업/00. Oreintation/01. orientation.py
@@ -0,0 +1,54 @@
+"""
+카페 : https://cafe.naver.com/an02
+
+계정 : ast22
+IP : 192.168.10.### (205 ~ 209)
+netmask : 255.255.255.0
+gateway : 192.168.10.1
+DNS : 8.8.8.8
+      192.168.10.11
+
+dal server : 192.168.10.11
+(http, ftp, samba, oracle, DNS)
+
+-----------------------------------필기-----------------------------------------
+그룹을 만드는 이유는 권한을 주기 위해서
+
+FTP Client - winscp(필요 용량이 적다)
+-- SourceForge : 검증이 안된 파일이라
+
+winscp 접속 (ftp 접속)
+      - 192.168.10.11
+      - ast / zaqxsw123
+
+/home2/ast/
+root -> home2 -> ast 디렉토리
+
+netmask 
+ - 같은 네트워크상의 있는 PC만 통신 가능
+ - ip address 가지고만 어느 영역에 속해있는지 확인 불가
+gateway
+ - 다른 네트워크 상의 있는 PC랑 통신하기 위한 주소
+DNS
+ - 
+
+ip는 컴퓨터에 셋팅하는게 아니라 랜카드(networkinteface)에 셋팅을 하는거다.
+
+인증서 - 공개키 기반구조 
+
+O/S : 하드웨어 매니지먼트
+booting : 커널에서 운영체제에 적재
+
+protection ring
+
+패킷 : 내부 정보를 외부로 보내는 것
+
+윈도우 사이즈
+- 혼잡제어 :
+- 흐름제어 : 
+
+정보시스템 구축
+1. erp (Enterprise Resource Planning)
+2. isp (Information Strategy Planning)
+3. ea (Enterprise Architecture)
+"""
