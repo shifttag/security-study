631fddf230f638d42ad716cc161e4477343f1259
shifttag <ehdgus0837@naver.com>
2026-06-22 17:26:15 +0900

    linux study 26.06.22

---
+++ 01. 수업/00. Oreintation/02. orientation_26.06.19.py
@@ -0,0 +1,78 @@
+"""
+26.06.19
+
+메인서버 소방점검 2년마다 그때마다 서버에만 발전기를 돌려 전원 연결해서 전체건물 shutdown
+
+NAT(Network Address Translation)
+공유기 마냥 ip 변환
+공유기 한대에서 PC 100대 사용해도 외부에서 보면 다 같은 ip이다
+
+터미널 에뮬레이터
+ex) putty
+
+putty
+
+(Appearance)
+Font Settings : 폰트 및 크기 지정
+
+(colours)
+Default Foreground : 글자 색
+Default Bold Foreground : 드래그 했을 때 글자 색
+Default Background : 배경 색
+Default Bold Background : 드래그 했을 때 배경 색
+
+unix
+드래그 자체가 복사
+이후 윈도우에서 Ctrl + V 하면 붙여넣어짐
+
+반대로 복사한거 unix에 붙여넣으려면 
+Shift + Insert | 우클릭 
+
+네트워크 종류
+Network edge
+- 일반적으로 사용하는 네트워크 
+Network core
+- 대규모 트래픽 전달
+
+정보보안 3대 요소
+- 기밀성
+- 무결성
+- 가용성
+
+클라우드
+
+proxymax 1차 hypervisor
+하드웨어에 직접 설치하는 것
+
+peer to peer(P2P) model
+: Client와 server 기능을 동시에 구현
+  - 과거 모델 사용이 안됨
+
+네트워크 교환
+Circuit switching(회선 교환)
+  : 통신 세션 동안 전용 회선(예약된 자원)을 이용하는 방법
+Packet switching(패킷 교환)
+  : 데이터를 일정 크기의 단위로 나누어 불연속적으로 네트워크에 전송하는 방법
+
+패킷은 헤더주소(보내는 주소, 보낼 주소)가 붙어있다
+
+VMWare 설치
+
+User Experience Settings에서 둘다 체크 해제
+
+
+
+### 네트워크 설명 ###
+C:\ | d:\ 두가지는 root 디렉토리이다
+백업 할 때 C:\ 디렉토리 아래에 DD 디렉토리를 생성하여
+D:\ 디렉토리랑 DD 디렉토리랑 마운트
+
+1. IP Adrress
+2. subnetmask
+3. gateway
+
+- 네트워크 주소가 일치하는 system과만 연결 가능 (IP, netmask)
+
+
+
+"""
